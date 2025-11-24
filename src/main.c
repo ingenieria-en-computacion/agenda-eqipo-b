f854bb08b2168dda2199335d40768dce1f804396
U-DESKTOP-J3KTAP1\estra <estradabarrerakaren@gmail.com>
2025-11-23 23:32:36 -0600

    se agregaron comentarios

--- src/main.c
+++ src/main.c
@@ -20,14 +20,14 @@ int main() {
         scanf("%d", &opcion);
 
         switch(opcion) {
-            case 1:
+            case 1: //lee el nombre del contaco agregado por el usuario y lo agrega a la agenda
                 printf("\n=== AGREGAR CONTACTO ===\n");
                 leer_contacto(&nuevo_contacto);
                 agregar_contacto(&agenda, nuevo_contacto);
                 printf("Contacto agregado exitosamente!\n");
                 break;
 
-            case 2:
+            case 2: //solicita un nombre al usuario, busca el contacto en la agenda y muestra sus datos si existe
                 printf("\n=== BUSCAR CONTACTO POR NOMBRE ===\n");
                 printf("Ingrese el nombre a buscar: ");
                 scanf("%s", nombre);
@@ -40,7 +40,7 @@ int main() {
                 }
                 break;
 
-            case 3:
+            case 3: //busca un contacto por su telefono y si existe, muestra su informacion
                 printf("\n=== BUSCAR CONTACTO X TELEFONO ===\n");
                 printf("Ingrese el telefono a buscar: ");
                 scanf("%s", telefono);
@@ -53,31 +53,31 @@ int main() {
                 }
                 break;
 
-            case 4:
+            case 4: //imprime todos los contactos guardados en la agenda 
                 printf("\n=== MOSTRAR TODOS LOS CONTACTOS ===\n");
                 imprimir_agenda(agenda);
                 break;
 
-            case 5:
+            case 5: // se ordenan los contactos en orden alfabetico de la A a la Z
                 printf("\n=== ORDENAR CONTACTOS (ASCENDENTE) ===\n");
                 ordenar_contactos(&agenda);
                 printf("Contactos ordenados alfabeticamente (A-Z)\n");
                 break;
 
-            case 6:
+            case 6: //ordena los contactos de la Z a la A
                 printf("\n=== ORDENAR CONTACTOS (DESCENDENTE) ===\n");
                 ordenar_contactos_inv(&agenda);
                 printf("Contactos ordenados alfabeticamente (Z-A)\n");
                 break;
 
-            case 7:
+            case 7: //el usuario escribe el nombre del archivo donde se guardaran los contactos
                 printf("\n=== GUARDAR CONTACTOS ===\n");
                 printf("Ingrese el nombre del archivo: ");
                 scanf("%s", filename);
                 guardar_contactos(filename, &agenda);
                 break;
 
-            case 8:
+            case 8: //carga en la agenda todos los contactos guardados en un archivo 
                 printf("\n=== CARGAR CONTACTOS ===\n");
                 printf("Ingrese el nombre del archivo: ");
                 scanf("%s", filename);
